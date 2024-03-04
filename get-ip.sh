44ed8d035d9673e1145107b95d664cec025049c7
Konstantin <91319844+fersaru@users.noreply.github.com>
2024-03-04 20:21:07 +0100

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -4,10 +4,13 @@ main () {
     local device_list_file="$(dirname "${BASH_SOURCE[0]}")/device-list"
     local device="$1"
 
+    # this is only used for demo purposes
+    local demo_lease_additions="--last --lease $(dirname "${BASH_SOURCE[0]}")/lease-example"
+
     local device_mac="$(tail -n +2 "${device_list_file}" | grep -m 1 -i "${device}" | awk '{print $1}')"
     [ -n "${device_mac}" ] || exit 1
   
-    local lease_list="$(dhcp-lease-list --last --parsable --lease lease-example 2>/dev/null)"
+    local lease_list="$(dhcp-lease-list --parsable ${demo_lease_additions} 2>/dev/null)"
     [ -n "${lease_list}" ] || exit 1
   
     local device_ip="$(echo "${lease_list}" | grep ${device_mac} | awk '{print $4}')"
